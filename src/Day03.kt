03fbef2f9859c9204fdc0e4df0823636f42f2723
flacchom <david.b.matejka@gmail.com>
2024-12-04 11:42:25 +0100

    replacing for loop with fold to calculate the sum; added regex solution to part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,15 +1,10 @@
 fun main() {
     fun part1(input: List<String>): Int {
         val regex = Regex("mul\\(([0-9]+),([0-9]+)\\)")
-        var sum = 0
-        for (line in input) {
-            val matches = regex.findAll(line)
-            matches.forEach {
-                sum += it.groupValues[1].toInt() * it.groupValues[2].toInt()
-            }
+        val matches = regex.findAll(input.joinToString())
+        return matches.fold(0) { sum, e ->
+            sum + (e.groupValues[1].toInt() * e.groupValues[2].toInt())
         }
-
-        return sum
     }
 
     fun getBeforeDont(line: String): Pair<String, String> {
@@ -31,13 +26,8 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        var text = ""
-        for (line in input) {
-            text += line
-        }
-
+        var erg = input.joinToString()
         var sum = 0
-        var erg = text
         while (true) {
             val prev = erg
             erg = removeInactiveParts(erg)
@@ -47,12 +37,21 @@ fun main() {
         return sum
     }
 
-    check(part1(listOf("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")) == 161)
-    check(part2(listOf("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")) == 96)
-    check(part2(listOf("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")) == 48)
+    fun part2Regex(input: List<String>): Int {
+        var text = input.joinToString()
+        text = "do()" + text + "don't()"
+        val matches = Regex("do\\(\\)(.*?)don't\\(\\)").findAll(text)
+        return matches.fold(0) { sum, e ->
+            sum + part1(e.groupValues.drop(1))
+        }
+    }
 
+    check(part1(listOf("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")) == 161)
+    check(part2(listOf("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")) == 48)
+    check(part2Regex(listOf("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")) == 48)
 
     val input = readInput("Day03")
     part1(input).println()
     part2(input).println()
+    part2Regex(input).println()
 }
