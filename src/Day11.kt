3936cc835bea39e4d47c2f6bd15200618556be84
flacchom <david.b.matejka@gmail.com>
2024-12-11 18:15:03 +0100

    refactored: "map" to "memo"; and condensed the variable "right"

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,27 +1,28 @@
-private val map = mutableMapOf<Pair<String, Int>, Long>()
+private val memo = mutableMapOf<Pair<String, Int>, Long>()
 fun main() {
 	fun applyRules(stone: String, blinksRemaining: Int): Long {
-		if (blinksRemaining == 0) return 1
-		if (Pair(stone, blinksRemaining) in map) return map[Pair(stone, blinksRemaining)]!!
-
+		if (blinksRemaining == 0) {
+			return 1
+		}
+		if (Pair(stone, blinksRemaining) in memo) {
+			return memo[Pair( stone, blinksRemaining )]!!
+		}
 		if (stone == "0") {
 			return applyRules("1", blinksRemaining - 1)
 		}
 
 		if (stone.length % 2 == 0) {
-			val left = stone.substring(0,stone.length / 2)
-			var right = stone.substring(stone.length / 2)
-			right = right.dropWhile { it == '0' }
-			if (right.isBlank()) right = "0"
+			val left = stone.substring(0, stone.length / 2)
+			var right = "${stone.substring(stone.length / 2).toInt()}"
 
 			val leftErg = applyRules(left, blinksRemaining - 1)
 			val rightErg = applyRules(right, blinksRemaining - 1)
 			val sum = leftErg + rightErg
-			map[Pair(stone, blinksRemaining)] = sum
+			memo[Pair(stone, blinksRemaining)] = sum
 			return sum
 		}
 
-		return applyRules("${stone.toLong()*2024}", blinksRemaining - 1)
+		return applyRules("${stone.toLong() * 2024}", blinksRemaining - 1)
 	}
 
 	fun part1(input: List<String>): Long {
@@ -47,7 +48,5 @@ fun main() {
 
 	check(part1(testInput) == 55312L)
 	part1(input).println()
-
-//	check(part2(testInput) == )
 	part2(input).println()
 }
