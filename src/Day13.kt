29277ade5a878b97f6f3557f393ca5e74beebc30
flacchom <david.b.matejka@gmail.com>
2024-12-13 19:36:32 +0100

    rewrote solution to part 1 since it's just a system of linear equations...

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,9 +1,7 @@
-import java.util.PriorityQueue
-
 fun main() {
 
-	data class Shift(val dx: Int, val dy: Int, val cost: Int)
-	data class Pos(val x: Int, val y: Int)
+	data class Shift(val dx: Long, val dy: Long, val cost: Long)
+	data class Pos(val x: Long, val y: Long)
 	data class Machine(val A: Shift, val B: Shift, val prize: Pos)
 
 	fun parseInput(input: List<String>): List<Machine> {
@@ -20,13 +18,13 @@ fun main() {
 		val machines = buildList {
 			blocks.forEach { block ->
 				val (dxA, dyA) = block[0].substringAfter(": ").split(", ").map {
-					it.substringAfter("+").toInt()
+					it.substringAfter("+").toLong()
 				}
 				val (dxB, dyB) = block[1].substringAfter(": ").split(", ").map {
-					it.substringAfter("+").toInt()
+					it.substringAfter("+").toLong()
 				}
 				val (x, y) = block[2].substringAfter(": ").split(", ").map {
-					it.substringAfter("=").toInt()
+					it.substringAfter("=").toLong()
 				}
 				val shiftA = Shift(dxA, dyA, 3)
 				val shiftB = Shift(dxB, dyB, 1)
@@ -37,54 +35,53 @@ fun main() {
 		return machines
 	}
 
+	fun calcFactors(machine: Machine): Pair<Long, Long> {
+		/*
 
-	fun calcSolVec(machine: Machine): Shift {
-		var solVec = Shift(0, 0, 0)
-		var i = 0
-		var foundSolution = false
-		var k = 0
-		var min = Int.MAX_VALUE
-		while (true) {
-			val b = (machine.prize.x - i * machine.A.dx) / machine.B.dx
-			if (i * machine.A.dx > machine.prize.x) {
-				break
-			}
-			if (foundSolution) {
-				k++
-			}
-			if (k == 1000000) {
-				break
-			}
-			if ((i * machine.A.dy + b * machine.B.dy).toInt() == machine.prize.y) {
-				if ((i * machine.A.dx + b * machine.B.dx) == machine.prize.x) {
-					val tmp = 3 * i + b
-					if (tmp < min) {
-						min = tmp.toInt()
-						solVec = Shift(
-							i * machine.A.dx + b * machine.B.dx,
-							i * machine.A.dy + b * machine.B.dy,
-							tmp
-						)
-					}
-					foundSolution = true
-				}
-			}
-			i++
-		}
-		if (min == Int.MAX_VALUE) {
-			min = 0
+		I	94 * a + 22 * b = 8400
+		II	34 * a + 67 * b = 5400
+	 I	machine.A.dx * i + machine.B.dx * j = machine.prize.x
+	 II	 machine.A.dy * i + machine.B.dy * j = machine.prize.y
+	 		I ax * a + bx * b = prize.x
+	 		I ay * a + by * b = prize.y
+
+		I 	94 * 34 * a + 22 * 34 * b = 8400 * 34
+		II	34 * 94 * a + 67 * 94 * b = 5400 * 94
+
+
+	II - I b = (5400 * 94 - 8400 * 34) / (67 * 94 - 22 * 34)
+					-> b = 40
+			b = (prize.y * ax - prize.x * ay) / (by * ax - bx * ay)
+
+		*/
+
+		val px = machine.prize.x
+		val py = machine.prize.y
+		val ax = machine.A.dx
+		val ay = machine.A.dy
+		val bx = machine.B.dx
+		val by = machine.B.dy
+
+		val b = (py * ax - px * ay) / (by * ax - bx * ay)
+		val a = (py - by * b) / ay
+
+		val first = ax * a + bx * b
+		val second = ay * a + by * b
+		if (first != px || second != py) {
+			return Pair(0, 0)
 		}
 
-		return Shift(machine.prize.x, machine.prize.y, min)
+		return Pair(a,b)
 	}
 
 
-	fun part1(input: List<String>): Int {
+	fun part1(input: List<String>): Long {
 		val machines = parseInput(input)
 
-		var sum = 0
+		var sum = 0L
 		machines.forEach { machine ->
-			sum += calcSolVec(machine).cost
+			val (a, b) = calcFactors(machine)
+			sum += 3 * a + b
 		}
 
 		return sum
@@ -92,32 +89,15 @@ fun main() {
 
 
 
-	fun part2(input: List<String>): Int {
+
+	fun part2(input: List<String>): Long {
 		val machines = parseInput(input)
-		var sum = 0
+		var sum = 0L
+
 		machines.forEach { machine ->
-			val solVec = calcSolVec(machine)
-			solVec.println()
-
-			val tmp = solVec.cost
-			val modX =
-				Math.floorMod(machine.prize.x + 10000000000000, machine.prize.x).toLong()
-			val fittingX =
-				(machine.prize.x + 10000000000000 - modX) / machine.prize.x
-
-			val modY =
-				Math.floorMod(machine.prize.y + 10000000000000, machine.prize.y).toLong()
-			val fittingY =
-				(machine.prize.y + 10000000000000 - modY) / machine.prize.y
-
-			modX.println()
-			modY.println()
-
-			fittingX.println()
-			fittingY.println()
-			val ggt = gcd(fittingX, fittingY)
-			"ggt: $ggt".println()
-
+			machine.println()
+			val (a, b) = calcFactors(machine)
+			sum += 3 * a + b
 		}
 
 		return sum
@@ -129,10 +109,10 @@ fun main() {
 
 	"part1:".println()
 //	part1(testInput).println()
-	check(part1(testInput) == 480)
+	check(part1(testInput) == 480L)
 	part1(input).println()
 
 	"part2:".println()
 	part2(testInput).println()
-//	part2(input).println()
+	part2(input).println()
 }
