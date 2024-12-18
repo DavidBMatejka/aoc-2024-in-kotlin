798240211d160abc19a5bb28a35cfffceb8e6aa0
flacchom <david.b.matejka@gmail.com>
2024-12-18 15:18:24 +0100

    added solutions for part 2 day 15

--- src/Day15.kt
+++ src/Day15.kt
@@ -7,8 +7,13 @@ fun main() {
 	)
 
 	data class Position(var x: Int, var y: Int)
+	data class GridElement(val x: Int, val y: Int, val char: Char)
 
-	fun MutableList<MutableList<Char>>.execute(move: Char, pos: Position): Position {
+	fun MutableList<MutableList<Char>>.execute(
+		move: Char,
+		pos: Position,
+		inPart2: Boolean = false,
+	): Position {
 		val (dx, dy) = dirs[move]!!
 
 		fun findFreeSpace(pos: Position): Position {
@@ -19,7 +24,9 @@ fun main() {
 				current.y += dy
 				currentChar = this[current.y][current.x]
 				when (currentChar) {
-					'#' -> { return pos }
+					'#' -> {
+						return pos
+					}
 				}
 
 			}
@@ -30,7 +37,8 @@ fun main() {
 			val current = Position(target.x, target.y)
 			var currentChar = this[target.y][target.x]
 			while (currentChar != '@') {
-				this[current.y][current.x] = this[current.y - dy][current.x - dx]
+				this[current.y][current.x] =
+					this[current.y - dy][current.x - dx]
 				current.x -= dx
 				current.y -= dy
 				currentChar = this[current.y][current.x]
@@ -39,11 +47,93 @@ fun main() {
 			return Position(current.x + dx, current.y + dy)
 		}
 
+		fun getElementsToPushVertical(pos: Position): Set<GridElement> {
+			val gridElements = mutableSetOf<GridElement>()
+
+			val currentChar = this[pos.y][pos.x]
+			when (currentChar) {
+				'@' -> {
+					gridElements.add(GridElement(pos.x, pos.y, '@'))
+				}
+				'#' -> {
+					return setOf(GridElement(-1, -1, '#'))
+				}
+				'.' -> {
+					return emptySet()
+				}
+				'[' -> {
+					gridElements.add(GridElement(pos.x, pos.y, '['))
+					gridElements.add(GridElement(pos.x + 1, pos.y, ']'))
+					gridElements.addAll(getElementsToPushVertical(Position(pos.x + 1, pos.y + dy)))
+				}
+				']' -> {
+					gridElements.add(GridElement(pos.x, pos.y, ']'))
+					gridElements.add(GridElement(pos.x - 1, pos.y, '['))
+					gridElements.addAll(getElementsToPushVertical(Position(pos.x - 1, pos.y + dy)))
+				}
+			}
+			gridElements.addAll(getElementsToPushVertical(Position(pos.x, pos.y + dy)))
+
+			return gridElements
+		}
+
+		fun getElementsToPushHorizontal(pos: Position): Set<GridElement> {
+			val gridElements = mutableSetOf<GridElement>()
+
+			val currentChar = this[pos.y][pos.x]
+			when (currentChar) {
+				'#' -> {
+					return setOf(GridElement(-1, -1, '#'))
+				}
+				'.' -> {
+					return emptySet()
+				}
+				else -> {
+					gridElements.add(GridElement(pos.x, pos.y, this[pos.y][pos.x]))
+					gridElements.addAll(getElementsToPushHorizontal(Position(pos.x + dx, pos.y)))
+				}
+			}
+
+			return gridElements
+		}
+
+		if (inPart2) {
+			if (move == '^' || move == 'v') {
+				val elementsToPush = getElementsToPushVertical(pos)
+				var newPos = pos
+				if (elementsToPush.none { it.char == '#' }) {
+					elementsToPush.forEach {
+						this[it.y][it.x] = '.'
+					}
+					elementsToPush.forEach {
+						if (it.char == '@') {
+							newPos = Position(it.x, it.y + dy)
+						}
+						this[it.y + dy][it.x] = it.char
+					}
+				}
+				return newPos
+			} else if (move == '>' || move == '<') {
+				val elementsToPush = getElementsToPushHorizontal(pos)
+				var newPos = pos
+				if (elementsToPush.none { it.char == '#' }) {
+					elementsToPush.forEach {
+						this[it.y][it.x] = '.'
+					}
+					elementsToPush.forEach {
+						if (it.char == '@') {
+							newPos = Position(it.x + dx, it.y)
+						}
+						this[it.y][it.x + dx] = it.char
+					}
+				}
+				return newPos
+			}
+		}
 		val freeSpacePosition = findFreeSpace(pos)
 		if (freeSpacePosition == pos) {
 			return pos
 		}
-
 		return walkBackAndMoveTo(freeSpacePosition)
 	}
 
@@ -59,6 +149,17 @@ fun main() {
 		return sum
 	}
 
+	fun MutableList<MutableList<Char>>.calcGPS2(): Long {
+		var sum = 0L
+		this.forEachIndexed { y, line ->
+			line.forEachIndexed { x, c ->
+				if (c == '[') {
+					sum += 100 * y + x
+				}
+			}
+		}
+		return sum
+	}
 
 	fun parseInput(input: List<String>): Pair<MutableList<MutableList<Char>>, String> {
 		val grid = mutableListOf<MutableList<Char>>()
@@ -104,14 +205,17 @@ fun main() {
 						tmp.add('#')
 						tmp.add('#')
 					}
+
 					'O' -> {
 						tmp.add('[')
 						tmp.add(']')
 					}
+
 					'.' -> {
 						tmp.add('.')
 						tmp.add('.')
 					}
+
 					'@' -> {
 						tmp.add('@')
 						tmp.add('.')
@@ -125,26 +229,16 @@ fun main() {
 	}
 
 
-	fun part2(input: List<String>): Int {
+	fun part2(input: List<String>): Long {
 		val (grid, moves) = parseInput(input)
 		val expandedGrid = grid.expand()
-		expandedGrid.forEach { it.joinToString("").println() }
 
 		var pos = getStartingPosition(expandedGrid)
-
-		pos = expandedGrid.execute('<', pos)
-		pos = expandedGrid.execute('<', pos)
-		pos = expandedGrid.execute('<', pos)
-		pos = expandedGrid.execute('<', pos)
-		pos = expandedGrid.execute('<', pos)
-		pos = expandedGrid.execute('<', pos)
-		pos = expandedGrid.execute('v', pos)
-		expandedGrid.forEach { it.joinToString("").println() }
 		for (move in moves) {
-//			pos = grid.execute(move, pos)
+			pos = expandedGrid.execute(move, pos, inPart2 = true)
 		}
 
-		return -1
+		return expandedGrid.calcGPS2()
 	}
 
 	val testInput = readInput("Day15_test")
@@ -154,7 +248,6 @@ fun main() {
 	check(part1(testInput) == 10092L)
 	part1(input).println()
 
-	part2(readInput("Day15_test_small2")).println()
-//	check(part2(testInput) == 9021)
-//	part2(input).println()
+	check(part2(testInput) == 9021L)
+	part2(input).println()
 }
