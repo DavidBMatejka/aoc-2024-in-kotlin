f2a54fc080d58adc481b06a9655ea3f12dd4910e
flacchom <david.b.matejka@gmail.com>
2024-12-19 12:57:30 +0100

    refactored part 1 and removed parseInput as inner function

--- src/Day19.kt
+++ src/Day19.kt
@@ -1,39 +1,37 @@
 fun main() {
     val dayNr = 19
 
+    fun parseInput(input: List<String>): Pair<List<String>, List<String>> {
+        val rules = input[0].split(", ")
+        val designs = input.dropWhile { it.isNotEmpty() }.drop(1)
+        return Pair(rules, designs)
+    }
+
     fun part1(input: List<String>): Int {
+        val (rules, designs) = parseInput(input)
+
         val mem = mutableMapOf<String, Boolean>()
-        var availablePatterns = listOf<String>()
-
-        fun parseInput(input: List<String>): List<String> {
-            availablePatterns = input[0].split(", ")
-            availablePatterns = availablePatterns.sortedByDescending { it.length }
-            val desiredDesigns = input.dropWhile { it.isNotEmpty() }.drop(1)
-            return desiredDesigns
-        }
-
-        fun checkTowel(pattern: String): Boolean {
+        fun checkTowel(pattern: String, rules: List<String>): Boolean {
             if (pattern == "") return true
             if (pattern in mem) return mem[pattern]!!
 
             mem[pattern] = false
-            for (w in availablePatterns) {
+            for (w in rules) {
                 val length = w.length
                 if (length <= pattern.length) {
                     val start = pattern.substring(0, length)
                     val rest = pattern.substring(length)
-                    if (start == w && checkTowel(rest)) {
+                    if (start == w && checkTowel(rest, rules)) {
                         mem[pattern] = true
                     }
                 }
             }
             return mem[pattern]!!
         }
-        val desiredDesigns = parseInput(input)
 
         var sum = 0
-        desiredDesigns.forEach { design ->
-            if (checkTowel(design) == true) {
+        designs.forEach { design ->
+            if (checkTowel(design, rules) == true) {
                 sum++
             }
         }
@@ -43,16 +41,17 @@ fun main() {
 
 
     fun part2(input: List<String>): Int {
-
         return -1
     }
 
     val testInput = readInput("Day${dayNr}_test")
     val input = readInput("Day${dayNr}")
 
-    check(part1(testInput) == 6)
-    part1(input).println()
-//
+//    check(part1(testInput) == 6)
+//    part1(input).println()
+
+    part2(testInput)
+
 //	check(part2(testInput) == -1)
 //    part2(input).println()
 }
