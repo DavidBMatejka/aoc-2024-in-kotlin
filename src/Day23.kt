31089842a640045d96a01ff2f31377c47dfe4b67
flacchom <david.b.matejka@gmail.com>
2024-12-23 21:49:20 +0100

    added solution to part 2 of day 23

--- src/Day23.kt
+++ src/Day23.kt
@@ -12,20 +12,25 @@ fun main() {
 		return graph.toSortedMap()
 	}
 
-	fun part1(input: List<String>): Int {
-		val graph = parseInput(input)
-		val interConnected = mutableSetOf<String>()
+	fun getInterconnected(graph: Map<String, Set<String>>): MutableSet<String> {
+		val interconnected = mutableSetOf<String>()
 		graph.forEach {
 			it.value.forEach { n1 ->
 				it.value.forEach { n2 ->
 					if (n1 != n2) {
 						if (n1 in graph[n2]!! && n2 in graph[n1]!!) {
-							interConnected.add(listOf(it.key, n1, n2).sorted().joinToString(","))
+							interconnected.add(listOf(it.key, n1, n2).sorted().joinToString(","))
 						}
 					}
 				}
 			}
 		}
+		return interconnected
+	}
+
+	fun part1(input: List<String>): Int {
+		val graph = parseInput(input)
+		val interConnected = getInterconnected(graph)
 
 		return interConnected.filter {
 			it.split(",").any {
@@ -34,9 +39,25 @@ fun main() {
 		}.size
 	}
 
-	fun part2(input: List<String>): Int {
+	fun part2(input: List<String>): String {
+		val graph = parseInput(input)
+		val interConnected = getInterconnected(graph)
 
-		return -1
+		val occurences = mutableMapOf<String, Int>()
+		interConnected.forEach {
+			val node = it.split(",").first()
+			occurences[node] = (occurences[node]?: 0) + 1
+		}
+
+		val mostFrequent = occurences.toList().maxByOrNull { it.second }!!.first
+		val erg = mutableSetOf<String>()
+		interConnected.filter {
+			it.split(",").first() == mostFrequent
+		}.forEach {
+			erg.addAll(it.split(","))
+		}
+
+		return erg.sorted().joinToString(",")
 	}
 
 	val testInput = readInput("Day${dayNr}_test")
@@ -45,6 +66,6 @@ fun main() {
 	check(part1(testInput) == 7)
 	part1(input).println()
 
-	check(part2(testInput) == -1)
+	check(part2(testInput) == "co,de,ka,ta")
 	part2(input).println()
 }
