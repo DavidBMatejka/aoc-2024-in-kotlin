3ab91bc0aa584d88d9ba60e251077cf87f59ebf3
flacchom <david.b.matejka@gmail.com>
2024-12-27 13:23:46 +0100

    removed bug in bfs (always used numpad for getting neighbours); added solution to part 2 day 21

--- src/Day21.kt
+++ src/Day21.kt
@@ -21,88 +21,113 @@ fun main() {
 	)
 
 	data class Pos(val x: Int, val y: Int, val path: String) {
-		fun getNeighbours(): List<Pos> {
+		fun getNeighbours(pad: List<String>): List<Pos> {
 			val neighbours = mutableListOf<Pos>()
 
 			for (dir in dirs) {
 				val (dx, dy) = dir.value
 				val next = Pos(x + dx, y + dy, path + dir.key)
-				if (next.y in numpad.indices && next.x in numpad[0].indices && numpad[next.y][next.x] != 'X') {
+				if (next.y in pad.indices && next.x in pad[0].indices && pad[next.y][next.x] != 'X') {
 					neighbours.add(next)
 				}
-			}
-			if (path.isNotEmpty()) {
-				neighbours.sortBy { it.path.last() != path.last() }
 			}
 			return neighbours
 		}
 
-		override fun equals(other: Any?): Boolean {
-			if (other !is Pos) return false
-			return (this.x == other.x && this.y == other.y)
-		}
-
-		override fun hashCode(): Int {
-			var result = x
-			result = 31 * result + y
-			return result
+		override fun toString(): String {
+			return "($x, $y)"
 		}
 	}
 
-	fun numpadCodeToDirections(code: String): String {
-		fun bfs(c: Char, start: Pos): Pos {
-			val q = mutableListOf<Pos>()
-			q.add(start)
-			val visited = mutableSetOf<Pos>()
+	fun bfs(start: Pos, end: Pos, pad: List<String>): List<String> {
+		if (start == end) return listOf("A")
 
-			while (q.isNotEmpty()) {
-				val current = q.removeFirst()
+		val paths = mutableListOf<String>()
+		var shortestPathLength = Int.MAX_VALUE
 
-				if (numpad[current.y][current.x] == c) {
-					return current
-				}
-				if (current in visited) continue
+		val q = mutableListOf(start)
 
-				val neighbours = current.getNeighbours()
-				for (n in neighbours) {
-					q.add(n)
-				}
+		while (q.isNotEmpty()) {
+			val current = q.removeFirst()
+			if (current.path.length > shortestPathLength) continue
 
+			if (current.x == end.x && current.y == end.y) {
+				paths.add(current.path + "A")
+				shortestPathLength = current.path.length
 			}
-			return Pos(0,0,"")
-		}
 
-		var start = Pos(2, 3, "")
-		return buildString {
-			for (c in code) {
-				val res = bfs(c, start)
-				append(res.path + "A")
-				start = Pos(res.x, res.y, "")
+			for (neighbour in current.getNeighbours(pad)) {
+				q.add(neighbour)
 			}
-		}.toString()
+		}
+		return paths
 	}
 
-	fun part1(input: List<String>): Int {
-		var sum = 0
-		for (line in input) {
-			numpadCodeToDirections(line).println()
+	fun generatePaths(pad: List<String>): Map<Pair<Char, Char>, List<String>> {
+		val mapping = mutableMapOf<Pair<Char, Char>, List<String>>()
+
+		for (y1 in pad.indices) {
+			for (x1 in pad[0].indices) {
+				val start = Pos(x1, y1, "")
+				val startChar = pad[y1][x1]
+				if (startChar == 'X') continue
+
+				for (y2 in pad.indices) {
+					for (x2 in pad[0].indices) {
+						val endChar = pad[y2][x2]
+						if (endChar == 'X') continue
+						val end = Pos(x2, y2, "")
+						val paths = bfs(start, end, pad)
+						mapping[Pair(startChar, endChar)] = paths
+					}
+				}
+			}
 		}
 
-		return sum
+		return mapping
 	}
 
-	fun part2(input: List<String>): Int {
+	val allNumpadPaths = generatePaths(numpad)
+	val allDirPaths = generatePaths(dirpad)
 
-		return -1
+	// checked todd ginsbergs solution for some help with the recursive function
+	// https://todd.ginsberg.com/post/advent-of-code/2024/day21/
+	fun calcCost(
+		code: String,
+		depth: Int,
+		transitions: Map<Pair<Char, Char>, List<String>> = allDirPaths,
+		mem: MutableMap<Pair<String, Int>, Long> = mutableMapOf(),
+	): Long {
+		return mem.getOrPut(Pair(code, depth)) {
+			"A$code".zipWithNext().sumOf {
+				val paths: List<String> = transitions.getValue(it)
+				if (depth == 0) {
+					paths.minOf { it.length }.toLong()
+				} else {
+					paths.minOf { path ->
+						calcCost(path, depth - 1, mem = mem)
+					}
+				}
+			}
+		}
+	}
+
+	fun part1(input: List<String>): Long {
+		return input.sumOf { line ->
+			calcCost(line, 2, allNumpadPaths) * line.dropLast(1).toLong()
+		}
+	}
+
+	fun part2(input: List<String>): Long {
+		return input.sumOf { line ->
+			calcCost(line, 25, allNumpadPaths) * line.dropLast(1).toLong()
+		}
 	}
 
 	val testInput = readInput("Day${dayNr}_test")
 	val input = readInput("Day${dayNr}")
 
-	part1(testInput).println()
-//	check(part1(testInput) == 126384)
-//	part1(input).println()
-//
-//	check(part2(testInput) == -1)
-//	part2(input).println()
+	check(part1(testInput) == 126384L)
+	part1(input).println()
+	part2(input).println()
 }
