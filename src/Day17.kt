64fd59cfc04b43087c0793f86a01dee725c61bee
flacchom <david.b.matejka@gmail.com>
2025-01-07 10:57:14 +0100

    added solution to part 2 of day 17

--- src/Day17.kt
+++ src/Day17.kt
@@ -3,15 +3,15 @@ import kotlin.math.pow
 fun main() {
 
 	data class State(
-		var a: Int,
-		var b: Int,
-		var c: Int,
+		var a: Long,
+		var b: Long,
+		var c: Long,
 		var pointer: Int,
 		val output: MutableList<Int> = mutableListOf<Int>(),
 		val program: List<Int>,
 	) {
 		fun execute(instr: Int, operand: Int) {
-			val comboOp = mapOf<Int, Int>(
+			val comboOp = mapOf<Int, Long>(
 				0 to 0,
 				1 to 1,
 				2 to 2,
@@ -21,33 +21,33 @@ fun main() {
 				6 to c,
 				7 to -1
 			)
+//			"a=$a, b=$b, c=$c, instr: $instr,$operand".println()
 			when (instr) {
 				0 -> {
 					var numerator = a
 					val exp = comboOp[operand]!!.toDouble()
 					val denominator = 2.0.pow(exp)
 					val erg = numerator / denominator
-					a = erg.toInt()
+					a = erg.toLong()
 					pointer += 2
 				}
 
 				1 -> {
-					b = b.xor(operand)
+					b = b.xor(operand.toLong())
 					pointer += 2
 				}
 
 				2 -> {
-					b = comboOp[operand]!!.mod(8)
+					b = comboOp[operand]!!.mod(8).toLong()
 					pointer += 2
 				}
 
 				3 -> {
-					if (a != 0) {
+					if (a != 0L) {
 						pointer = operand
 					} else {
-						pointer += 1
+						pointer += 2
 					}
-
 				}
 
 				4 -> {
@@ -65,7 +65,7 @@ fun main() {
 					val exp = comboOp[operand]!!.toDouble()
 					val denominator = 2.0.pow(exp)
 					val erg = numerator / denominator
-					b = erg.toInt()
+					b = erg.toLong()
 					pointer += 2
 				}
 
@@ -74,7 +74,7 @@ fun main() {
 					val exp = comboOp[operand]!!.toDouble()
 					val denominator = 2.0.pow(exp)
 					val erg = numerator / denominator
-					c = erg.toInt()
+					c = erg.toLong()
 					pointer += 2
 				}
 			}
@@ -90,9 +90,9 @@ fun main() {
 	}
 
 	fun part1(input: List<String>): String {
-		val a = input[0].substringAfter(": ").toInt()
-		val b = input[1].substringAfter(": ").toInt()
-		val c = input[2].substringAfter(": ").toInt()
+		val a = input[0].substringAfter(": ").toLong()
+		val b = input[1].substringAfter(": ").toLong()
+		val c = input[2].substringAfter(": ").toLong()
 		val program = input.last().substringAfter(": ").split(",").map { it.toInt() }
 
 		val state = State(a, b, c, 0, program = program)
@@ -101,9 +101,49 @@ fun main() {
 		return state.output.joinToString(",")
 	}
 
-	fun part2(input: List<String>): Int {
+	fun part1AsTest(a: Long): List<Int> {
+		val program = listOf(2,4,1,2,7,5,0,3,1,7,4,1,5,5,3,0)
+		val state = State(a, 0, 0, 0, program = program)
+		state.run()
+		return state.output
+	}
 
-		return -1
+
+
+	fun part2(input: List<String>): Long {
+		val program = input.last().substringAfter(": ").split(",").map { it.toInt() }
+
+		fun getWays(start: Long, goalList: List<Int>): Set<Long> {
+			return buildSet {
+				for (i in 0..7L) {
+					val a = (8 * start) + i
+					var tested = part1AsTest(a)
+					if (tested == goalList) {
+						add(a)
+					}
+				}
+			}
+		}
+
+		fun getWayRec(
+			start: Long = 0L,
+			currentList: List<Int> = listOf(0),
+			index: Int = program.size - 2,
+		): List<Long> {
+			val solutions = mutableListOf<Long>()
+			getWays(start, currentList).forEach {
+				if (currentList.size < program.size) {
+					val nextList = currentList.toMutableList().apply { addFirst(program[index]) }
+					solutions.addAll(getWayRec(it, nextList, index = index - 1))
+				}
+				if (currentList == program) {
+					solutions.add(it)
+				}
+			}
+			return solutions
+		}
+
+		return getWayRec().min()
 	}
 
 	val testInput = readInput("Day17_test")
@@ -111,7 +151,5 @@ fun main() {
 
 	check(part1(testInput) == "4,6,3,5,6,3,5,2,1,0")
 	part1(input).println()
-
-	check(part2(testInput) == -1)
 	part2(input).println()
 }
